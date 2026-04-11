5290285ba32c44d35ba806ccbe60f049037343da
Wibi <Wibi@gmail.com>
2026-04-11 18:54:34 +0000

    DS Task 9 2nd

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-
+"""Module to find the max integer in a list"""
 
 
 def max_integer(my_list=[]):
-
+    """Finds the biggest integer of a list."""
     if not my_list:
-
         return None
 
-
-
     max_val = my_list[0]
-
     for i in my_list:
-
         if i > max_val:
-
             max_val = i
-
     return max_val
