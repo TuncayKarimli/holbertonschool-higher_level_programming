5f52805aa21f04580e05fb5ac26a5945ccf38b7f
Wibi <Wibi@gmail.com>
2026-04-17 19:45:11 +0000

    Network2 task 0 fix

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-This module fetches a specific URL using the urllib package.
-It displays the type, content, and utf-8 decoded value of the response.
+This module fetches https://intranet.hbtn.io/status using urllib.
+The response body is printed with specific formatting and indentation.
 """
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = 'https://intranet.hbtn.io/status'
     req = urllib.request.Request(url)
     req.add_header('cfclearance', 'true')
 
     with urllib.request.urlopen(req) as response:
-        content = response.read()
+        body = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(content)))
-        print("    - content: {}".format(content))
-        print("    - utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
